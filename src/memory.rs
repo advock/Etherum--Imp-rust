d3c8488add83b19e976290ef1547e35edd5c9739
ADVK <advaiyakarnik@gmail.com>
2023-02-20 13:54:30 +0530

    4th mem

--- src/memory.rs
+++ src/memory.rs
@@ -60,4 +60,100 @@ impl Memory {
 
         Ok(())
     }
+
+    pub fn get(&self, offset: usize, size: usize) -> Vec<u8> {
+        let mut x = Vec::new();
+        x.resize(size, 0);
+
+        #[allow(clippy::needless_range_loop)]
+        for index in 0..size {
+            let position = offset + index;
+            if position >= self.data.len() {
+                break;
+            }
+
+            ret[index] = self.data[position];
+        }
+        x
+    }
+    pub fn set(
+		&mut self,
+		offset: usize,
+		value: &[u8],
+		target_size: Option<usize>,
+	) -> Result<(), ExitFatal> {
+		let target_size = target_size.unwrap_or(value.len());
+		if target_size == 0 {
+			return Ok(());
+		}
+
+		if offset
+			.checked_add(target_size)
+			.map(|pos| pos > self.limit)
+			.unwrap_or(true)
+		{
+			return Err(ExitFatal::NotSupported);
+		}
+
+		if self.data.len() < offset + target_size {
+			self.data.resize(offset + target_size, 0);
+		}
+
+		if target_size > value.len() {
+			self.data[offset..((value.len()) + offset)].clone_from_slice(value);
+			for index in (value.len())..target_size {
+				self.data[offset + index] = 0;
+			}
+		} else {
+			self.data[offset..(target_size + offset)].clone_from_slice(&value[..target_size]);
+		}
+
+		Ok(())
+	}
+
+	pub fn copy_large(
+		&mut self,
+		memory_offset: U256,
+		data_offset: U256,
+		len: U256,
+		data: &[u8],
+	) -> Result<(), ExitFatal> {
+		
+		if len.is_zero() {
+			return Ok(());
+		}
+
+		let memory_offset = if memory_offset > U256::from(usize::MAX) {
+			return Err(ExitFatal::NotSupported);
+		} else {
+			memory_offset.as_usize()
+		};
+
+		let ulen = if len > U256::from(usize::MAX) {
+			return Err(ExitFatal::NotSupported);
+		} else {
+			len.as_usize()
+		};
+
+		let data = if let Some(end) = data_offset.checked_add(len) {
+			if end > U256::from(usize::MAX) {
+				&[]
+			} else {
+				let data_offset = data_offset.as_usize();
+				let end = end.as_usize();
+
+				if data_offset > data.len() {
+					&[]
+				} else {
+					&data[data_offset..min(end, data.len())]
+				}
+			}
+		} else {
+			&[]
+		};
+
+		self.set(memory_offset, data, Some(ulen))
+	}
+}
+
 }
